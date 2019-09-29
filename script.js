7b3a722c7d37c70be3f0d149a17f1ab74783b008
vinnav <vincenzo.navarra@gmail.com>
2019-09-29 13:58:26 +0100

    added main logic and functions

--- script.js
+++ script.js
@@ -47,95 +47,188 @@ function cancelFunction(){
 }
 
 function oneFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let one = document.createTextNode("1");
     document.getElementById("resultP").appendChild(one);
 }
 
 function twoFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let two = document.createTextNode("2");
     document.getElementById("resultP").appendChild(two);
 }
 
 function threeFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let three = document.createTextNode("3");
     document.getElementById("resultP").appendChild(three);
 }
 
 function fourFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let four = document.createTextNode("4");
     document.getElementById("resultP").appendChild(four);
 }
 
 function fiveFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let five = document.createTextNode("5");
     document.getElementById("resultP").appendChild(five);
 }
 
 function sixFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let six = document.createTextNode("6");
     document.getElementById("resultP").appendChild(six);
 }
 
 function sevenFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let seven = document.createTextNode("7");
     document.getElementById("resultP").appendChild(seven);
 }
 
 function eightFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let eight = document.createTextNode("8");
     document.getElementById("resultP").appendChild(eight);
 }
 
 function nineFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let nine = document.createTextNode("9");
     document.getElementById("resultP").appendChild(nine);
 }
 
 function zeroFunction(){
+    if(resetDisplay){
+        cancelFunction();
+        resetDisplay = false;
+    }
     let zero = document.createTextNode("0");
     document.getElementById("resultP").appendChild(zero);
 }
 
+var signStatus = false;
 function mplusFunction(){
-
-
+    //if (sign == document.createTextNode("-")){
+   //     document.getElementById("resultP").innerHTML = document.getElementById("resultP").textContent.substring(1);
+  //  } else {
+    if(signStatus == true){
+        document.getElementById("resultP").innerHTML = document.getElementById("resultP").textContent.substring(1);
+        signStatus = false;
+    } else {
+    let sign = document.createTextNode("-");
+    document.getElementById("resultP").prepend(sign);
+    signStatus = true;
+    }
+ //   }
 }
 
 function percentFunction(){
-    alert("percent");
+    divisionFunction()
+    document.getElementById("resultP").textContent = "100";
+    equalFunction()
+
 }
 
+
+//operations
+var operation;
+var firstValue;
+var secondValue;
+var total;
+var resetDisplay;
+
 function divisionFunction(){
-    alert("division");
+    operation = "division";
+    resetDisplay = true;
+    firstValue = document.getElementById("resultP").textContent;
 }
 
 function multiplyFunction(){
-    alert("multiply");
+    operation = "multiply";
+    resetDisplay = true;
+    firstValue = document.getElementById("resultP").textContent;
 }
 
 function minusFunction(){
-    alert("minus");
+    operation = "minus";
+    resetDisplay = true;
+    firstValue = document.getElementById("resultP").textContent;
 }
 
 function plusFunction(){
-    let plusStatus = true;
-    let firstValue = document.getElementById("resultP").textContent;
-    cancelFunction();
+    operation = "plus";
+    resetDisplay = true;
+    firstValue = document.getElementById("resultP").textContent;
+    //alert(firstValue + " saved");
+
 }
 
 function dotFunction(){
+    if(document.getElementById("resultP").textContent.includes(".")){
+
+    } else {
     let dot = document.createTextNode(".");
     document.getElementById("resultP").appendChild(dot);
+    }
 }
 
 function equalFunction(){
+    if(operation == "plus"){
+        secondValue = document.getElementById("resultP").textContent;
+        total = parseFloat(firstValue)+parseFloat(secondValue);
 
-//    secondValue = document.getElementById("resultP").textContent;
-  //  let total = 0;
-    //if(plusStatus==true){
-  //      total = parseInt(firstValue) + parseInt(secondValue);
-   //     alert(total);
-   // }
+        document.getElementById("resultP").innerHTML = total;
+    }
+    if(operation == "minus"){
+        secondValue = document.getElementById("resultP").textContent;
+        total = parseFloat(firstValue)-parseFloat(secondValue);
 
+        document.getElementById("resultP").innerHTML = total;
+    }
+    if(operation == "multiply"){
+        secondValue = document.getElementById("resultP").textContent;
+        total = parseFloat(firstValue)*parseFloat(secondValue);
 
+        document.getElementById("resultP").innerHTML = total;
+    }
+    if(operation == "division"){
+        secondValue = document.getElementById("resultP").textContent;
+        if(parseInt(secondValue) == 0){
+            document.getElementById("resultP").innerHTML = "ERROR";
+        }
+        total = parseInt(firstValue)/parseInt(secondValue);
+        total = total.toFixed(2);
+
+        document.getElementById("resultP").innerHTML = total;
+    }
 }
